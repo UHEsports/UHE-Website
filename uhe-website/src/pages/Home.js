c674db42e85e7336ebe651ea4bef583fd1bcf48c
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2023-09-02 22:15:22 -1000

    Added general homepage layout still working on the about us page

--- uhe-website/src/pages/Home.js
+++ uhe-website/src/pages/Home.js
@@ -1,144 +1,81 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
+import './Home.css';
 import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-import logo from "../logo.svg";
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import group_1 from '../images/group_1.jpg'
+import group_2 from '../images/group_2.jpg'
+import group_3 from '../images/group_3.jpg'
+import group_4 from '../images/group_4.png'
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+function HomePageImage() {
+    return(
+        <div>
+            {/*<img*/}
+            {/*    width="100%"*/}
+            {/*    src={group_4}*/}
+            {/*/>*/}
+            <div id="group4">
 
-const images = [
-    {
-        label: 'San Francisco – Oakland Bay Bridge, United States',
-        imgPath:
-            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Bird',
-        imgPath:
-            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Bali, Indonesia',
-        imgPath:
-            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-    },
-    {
-        label: 'Goč, Serbia',
-        imgPath:
-            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-];
+            </div>
+        </div>
+    );
+}
+function AboutSection() {
+    return (
+        <Grid sx={{mt: 4}} container>
+            <Grid item xs={5}>
+                <div id="group3">
+                    {/*test*/}
+                </div>
+            </Grid>
+            <Grid item xs={7}>
+                <Card
+                    sx={{ p: 3, minWidth: 275 }}
+                    style={{backgroundColor: '#F6F6F6'}}
+                    variant="outlined" >
+                    <CardContent>
+                        <Typography
+                            sx={{textAlign:'center', fontWeight:'bold'}}
+                            variant="h4"
+                            component="div">
+                            About Us
+                        </Typography>
+                        <Typography
+                            sx={{textAlign:'left'}}
+                            variant="h5"
+                            component="div">
+                            UH Esports is a college esports program that draws on the strengths of our community of mentors,
+                            educators, and alumni to provide students with <span style={{fontWeight:'bold'}}>opportunities</span> for collegiate competition
+                            in esports, career paths into the wider video game industry, and a space tat welcomes the <span style={{fontWeight:'bold'}}>diversity</span> of students on all UH campuses
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    );
+}
 
 function Home() {
-    const theme = useTheme();
-    const [activeStep, setActiveStep] = React.useState(0);
-    const maxSteps = images.length;
+    const [scrollTop, setScrollTop] = React.useState(0);
+    const handleScroll = () => setScrollTop(window.scrollY);
+    React.useEffect(() =>{
+        window.addEventListener("scroll", handleScroll);
 
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleStepChange = (step) => {
-        setActiveStep(step);
-    };
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
     return (
         <div>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <Button variant="contained" className="changeColor">Hello World</Button>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-            <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-                <Paper
-                    square
-                    elevation={0}
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        height: 50,
-                        pl: 2,
-                        bgcolor: 'background.default',
-                    }}
-                >
-                    <Typography>{images[activeStep].label}</Typography>
-                </Paper>
-                <AutoPlaySwipeableViews
-                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                    index={activeStep}
-                    onChangeIndex={handleStepChange}
-                    enableMouseEvents
-                >
-                    {images.map((step, index) => (
-                        <div key={step.label}>
-                            {Math.abs(activeStep - index) <= 2 ? (
-                                <Box
-                                    component="img"
-                                    sx={{
-                                        height: 255,
-                                        display: 'block',
-                                        maxWidth: 400,
-                                        overflow: 'hidden',
-                                        width: '100%',
-                                    }}
-                                    src={step.imgPath}
-                                    alt={step.label}
-                                />
-                            ) : null}
-                        </div>
-                    ))}
-                </AutoPlaySwipeableViews>
-                <MobileStepper
-                    steps={maxSteps}
-                    position="static"
-                    activeStep={activeStep}
-                    nextButton={
-                        <Button
-                            size="small"
-                            onClick={handleNext}
-                            disabled={activeStep === maxSteps - 1}
-                        >
-                            Next
-                            {theme.direction === 'rtl' ? (
-                                <KeyboardArrowLeft />
-                            ) : (
-                                <KeyboardArrowRight />
-                            )}
-                        </Button>
-                    }
-                    backButton={
-                        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                            {theme.direction === 'rtl' ? (
-                                <KeyboardArrowRight />
-                            ) : (
-                                <KeyboardArrowLeft />
-                            )}
-                            Back
-                        </Button>
-                    }
-                />
-            </Box>
+
+            <HomePageImage />
+            <AboutSection />
+
         </div>
 
     );
