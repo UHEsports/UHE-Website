9fbe1fd339a0276cb5ab82fc1c5826b6e07856ed
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2023-09-15 21:17:23 -1000

    added a space to a function parameter

--- uhe-website/src/components/SectionDescription.js
+++ uhe-website/src/components/SectionDescription.js
@@ -1,7 +1,7 @@
 import {styled} from "@mui/material/styles";
 
 // Use this component like a div box
-const SectionDescription = styled('div')(({ theme}) => ({
+const SectionDescription = styled('div')(({ theme }) => ({
     textAlign: 'left',
     letterSpacing: "0.15px",
     fontSize: "1.5rem",
