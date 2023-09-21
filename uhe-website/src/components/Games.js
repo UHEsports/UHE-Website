01e4fc1514328763d35de2184010005e74390d77
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2023-09-20 23:52:01 -1000

    added in more backgrounds for different textures for the various games

--- uhe-website/src/components/Games.js
+++ uhe-website/src/components/Games.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import {styled} from "@mui/material/styles";
-import apex from '../images/wraith_apex.png'
+import apex from '../images/wraith_apex.png';
+import valorant from '../images/jett_valorant.webp';
+import league from '../images/ahri_lol.png';
+import worldsEdge from '../images/worlds_edge_v2_1.png';
+import summonersRift from '../images/summoners_rift_1.png';
+import haven from '../images/haven_1.png';
 import SectionTitle from "./SectionTitle";
 import {useInView} from "react-intersection-observer";
 import './Games.css';
 
 
-const GameImage = styled('div')(({ theme }) => ({
+const ApexImage = styled('div')(({ theme }) => ({
     backgroundSize: "contain",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "-40px 20px",
@@ -15,7 +20,6 @@ const GameImage = styled('div')(({ theme }) => ({
     height: '461px',
     transition: 'all .7s',
     position:'absolute'
-
     // [theme.breakpoints.down('lg')]: {
     //     height: "300px",
     //     [theme.breakpoints.down('md')]: {
@@ -24,14 +28,59 @@ const GameImage = styled('div')(({ theme }) => ({
     // },
 }));
 
+const ValorantImage = styled('div')(({ theme }) => ({
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "-60px 20px",
+    width: '270px',
+    height: '414px',
+    transition: 'all .7s',
+    position:'absolute'
+    // [theme.breakpoints.down('lg')]: {
+    //     height: "300px",
+    //     [theme.breakpoints.down('md')]: {
+    //         height: "250px",
+    //     },
+    // },
+}));
+
+const LOLImage = styled('div')(({ theme }) => ({
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "-220px 5px",
+    width: '600px',
+    height: '431px',
+    transition: 'all .7s',
+    position:'absolute'
+    // [theme.breakpoints.down('lg')]: {
+    //     height: "300px",
+    //     [theme.breakpoints.down('md')]: {
+    //         height: "250px",
+    //     },
+    // },
+}));
+
+const GameCard = styled('div')(({theme, background}) => ({
+    width: '100px',
+    height: '400px',
+    overflow: 'hidden',
+    borderRadius:'10px',
+    position: 'relative',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundImage: `url('${background}')`,
+    color:'rgba(255,255,255,0)',
+    transition: 'background-image .5s, color .5s',
+}));
+
 const GameTitleText = styled('div')(({ theme }) => ({
     fontWeight: 'bold',
-    fontSize: "2.5rem",
+    fontSize: "2.30rem",
     textAlign:'left',
     transform: 'rotate(-90deg)',
-    width: '400px',
-    top: '43%',
-    left:'-120px',
+    width: '420px',
+    top: '40%',
+    left:'-130px',
     position:'absolute',
     // transition: 'opacity .7s',
 }));
@@ -48,60 +97,25 @@ function Games() {
                 </SectionTitle>
             </Grid>
 
-            <div className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
+            <GameCard className="parent" background={worldsEdge}>
+                <ApexImage className="child" style={{backgroundImage: `url('${apex}')`}} />
                 <GameTitleText>
                     APEX LEGENDS
                 </GameTitleText>
-            </div>
-            <div style={{marginLeft:'10px'}} className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
+            </GameCard>
+
+            <GameCard style={{marginLeft:'10px'}} background={haven} className="parentValorant">
+                <ValorantImage className="child" style={{backgroundImage: `url('${valorant}')`}} />
                 <GameTitleText>
-                    APEX LEGENDS
+                    VALORANT
                 </GameTitleText>
-            </div>
-            <div style={{marginLeft:'10px'}} className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
+            </GameCard>
+            <GameCard style={{marginLeft:'10px'}} background={summonersRift} className="parentLeague">
+                <LOLImage className="child" style={{backgroundImage: `url('${league}')`}} />
                 <GameTitleText>
-                    APEX LEGENDS
+                    LEAGUE OF LEGENDS
                 </GameTitleText>
-            </div>
-            <div style={{marginLeft:'10px'}} className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
-                <GameTitleText>
-                    APEX LEGENDS
-                </GameTitleText>
-            </div>
-            <div style={{marginLeft:'10px'}} className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
-                <GameTitleText>
-                    APEX LEGENDS
-                </GameTitleText>
-            </div>
-            <div style={{marginLeft:'10px'}} className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
-                <GameTitleText>
-                    APEX LEGENDS
-                </GameTitleText>
-            </div>
-            <div style={{marginLeft:'10px'}} className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
-                <GameTitleText>
-                    APEX LEGENDS
-                </GameTitleText>
-            </div>
-            <div style={{marginLeft:'10px'}} className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
-                <GameTitleText>
-                    APEX LEGENDS
-                </GameTitleText>
-            </div>
-            <div style={{marginLeft:'10px'}} className="parent">
-                <GameImage className="child" style={{backgroundImage: `url('${apex}')`}} />
-                <GameTitleText>
-                    APEX LEGENDS
-                </GameTitleText>
-            </div>
+            </GameCard>
         </Grid>
 
     );
