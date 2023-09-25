7a42098370166fb98d0fbb61452d008635dfc729
Kmiks <143069244+Kmiks@users.noreply.github.com>
2023-09-24 14:06:06 -1000

    IlabPageImage created
    Created the IlabPageImage to make it consistant with other pages. Along with creating an empty function for the space and available equipment

--- src/pages/Ilab.js
+++ src/pages/Ilab.js
@@ -1,30 +1,37 @@
-import * as React from 'react';
+/*import * as React from 'react';
 import logo from "../logo.svg";
 import Button from "@mui/material/Button";
-import { useInView } from 'react-intersection-observer';
+import { useInView } from 'react-intersection-observer';*/
 //importing everything 
+import * as React from 'react';
+import './Home.css';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import group_1 from '../images/group_1.jpg';
+import group_2 from '../images/group_2.jpg';
+import group_3 from '../images/group_3.jpg';
+import group_4 from '../images/group_4.png';
+import discord_server from '../images/discord_server.png';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import uheLogo from "../images/uhe_logo.png";
+import SectionTitle from "../components/SectionTitle";
+import SectionDescription from "../components/SectionDescription";
+import {Link as RouterLink} from "react-router-dom";
+import OpeningPageImage from '../components/OpeningPageImage';
 
-function IlabPageImage(){
+function Spaces(){
+
+}
+
+function AvailableEquipment(){
 
 }
 
 function Ilab() {
     return (
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <Button variant="contained" className="changeColor">Hello World</Button>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
+        <OpeningPageImage header="iLab" subtitle="Subtitle text" imageURL={group_4}/>
     );
 }
 
