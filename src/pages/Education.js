ca21d4c98c02b375fdb4b1424f7e7b752dd6d8bb
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2023-09-24 23:49:04 -1000

    The lg internship portion has been finished

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -5,27 +5,25 @@ import OpeningPageImage from "../components/OpeningPageImage";
 import Grid from '@mui/material/Grid';
 import Slider from "react-slick";
 import education_1 from "../images/education_intro_image_1.png";
-import education_2 from "../images/education_intro_image_2.png";
-import education_3 from "../images/education_intro_image_3.png";
-import group_4 from "../images/group_4.png";
 import gen_g_background from '../images/gen_g_grey_background.png';
 import SectionTitle from "../components/SectionTitle";
 import SectionDescription from "../components/SectionDescription";
 import {styled} from "@mui/material";
-import group_1 from "../images/group_1.jpg";
-import group_2 from "../images/group_2.jpg";
-import group_3 from "../images/group_3.jpg";
+import geng_1 from "../images/geng_1.png";
+import geng_2 from "../images/geng_2.png";
+import geng_3 from "../images/geng_3.png";
 
 const InternshipBox = styled('div')(({ theme, imageURL }) => ({
     backgroundImage: `url('${imageURL}')`,
     backgroundSize: "cover",
     width:'1130px',
     height: "579px",
-    display: "flex",
-    justifyContent: 'center',
-    // alignItems: "center",
-    alignContent: 'flex-start',
     position:'relative',
+    // display: "flex",
+    // justifyContent: 'center',
+    // alignItems: "center",
+    // alignContent: 'flex-start',
+
     // [theme.breakpoints.down('lg')]: {
     //     height: "600px",
     //     [theme.breakpoints.down('sm')]: {
@@ -34,7 +32,8 @@ const InternshipBox = styled('div')(({ theme, imageURL }) => ({
     // },
 }));
 
-const IntershipCarouselPosition = styled('div')(({ theme, imageURL }) => ({
+// used as div instead of a class for responsiveness options
+const InternshipCarouselPosition = styled('div')(({ theme, imageURL }) => ({
     position: 'absolute',
     top: '25px',
     left: '800px',
@@ -43,9 +42,10 @@ const IntershipCarouselPosition = styled('div')(({ theme, imageURL }) => ({
 
 const CarouselImages = styled('div')(({ theme }) => ({
     backgroundSize: "cover",
+    backgroundPosition:"center",
     height: "600px",
-    borderRadius:'10px',
-    margin: '20px 0 20px 20px',
+    // borderRadius:'10px',
+    // margin: '20px 0 20px 20px',
     // [theme.breakpoints.down('lg')]: {
     //     height: "300px",
     //     [theme.breakpoints.down('md')]: {
@@ -54,8 +54,8 @@ const CarouselImages = styled('div')(({ theme }) => ({
     // },
 }));
 
-const settings = {
-    dots: true,
+const imageCarouselSettings = {
+    dots: false,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -64,13 +64,24 @@ const settings = {
     arrows:false,
 };
 
+const textCarouselSettings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    arrows:true,
+};
+
 const images = [
-    group_1,
-    group_2,
-    group_3,
-    group_1,
-    group_2,
-    group_3
+    geng_1,
+    geng_2,
+    geng_3,
+];
+
+const gengText = [
+    "Learn from the Pros: Duis in nulla ullamco cillum aliqua et incididunt in culpa tempor cupidatat irure consectetur est aliquip dolor, amet. Aute, adipisicing amet quis sit voluptate reprehenderit elit occaecat ad quis magna culpa anim dolor do aliqua duis nulla ad.",
+    "Experience Korean Culture: Duis in nulla ullamco cillum aliqua et incididunt in culpa tempor cupidatat irure consectetur est aliquip dolor, amet. Aute, adipisicing amet quis sit voluptate reprehenderit elit occaecat ad quis magna culpa anim dolor do aliqua duis nulla ad."
 ];
 
 function Internships() {
@@ -79,7 +90,7 @@ function Internships() {
         rootMargin: '-50px 0px',
     });
     return (
-        <Grid container sx={{mt:{ lg:5, md:2, sm:1 }}} justifyContent="center" ref={ref}>
+        <Grid container sx={{mt:{ lg:6, md:2, sm:1 }}} justifyContent="center" ref={ref}>
             <Grid item md={12}>
                 <SectionTitle className={inView ? 'slideIn1300ms' : 'hidden'}>
                     Internships
@@ -92,13 +103,30 @@ function Internships() {
                     Aute, adipisicing amet quis sit voluptate reprehenderit elit occaecat ad quis magna culpa anim
                 </SectionDescription>
             </Grid>
-            <Grid item md={12} sx={{mt:{ lg:4, md:2, sm:1 }}}>
-                <InternshipBox imageURL={gen_g_background}>
-                    <SectionTitle className={inView ? 'slideIn1300ms' : 'hidden'}>
-                        GEN G
-                    </SectionTitle>
-                    <IntershipCarouselPosition>
-                        <Slider {...settings}>
+            <Grid item md={12} sx={{mt:{ lg:6, md:2, sm:1 }}}>
+                <InternshipBox imageURL={gen_g_background} sx={{display:'flex', alignItems:'center'}}>
+                    <Grid container>
+                        <Grid item md={8} sx={{p:3, ml:2}}>
+                            <SectionTitle className={inView ? 'slideIn1300ms' : 'hidden'}>
+                                GEN G
+                            </SectionTitle>
+                                <Slider {...textCarouselSettings}>
+                                    {
+                                        gengText.map((text) => {
+                                            return(
+                                                <>
+                                                    <SectionDescription sx={{p:2, textAlign:'center'}}>
+                                                        {text}
+                                                    </SectionDescription>
+                                                </>
+                                            );
+                                        })
+                                    }
+                                </Slider>
+                        </Grid>
+                    </Grid>
+                    <InternshipCarouselPosition>
+                        <Slider {...imageCarouselSettings}>
                             {
                                 images.map((image) => {
                                     return(
@@ -109,7 +137,7 @@ function Internships() {
                                 })
                             }
                         </Slider>
-                    </IntershipCarouselPosition>
+                    </InternshipCarouselPosition>
                 </InternshipBox>
             </Grid>
         </Grid>
