e3a2467439263c2df069d25304c0889382f864cf
Kmiks <143069244+Kmiks@users.noreply.github.com>
2023-10-03 12:50:25 -1000

    edited code for available equipment

--- src/pages/Ilab.js
+++ src/pages/Ilab.js
@@ -21,7 +21,7 @@ import {Link as RouterLink} from "react-router-dom";
 import OpeningPageImage from '../components/OpeningPageImage';
 import { Block, ChildCare } from '@mui/icons-material';
 
-const SpacesMainImage = styled('images')(({ theme }) => ({
+const LocationMainText = styled('images')(({ theme }) => ({
     backgroundSize: "cover",
     height: "250px",
     [theme.breakpoints.down('lg')]: {
@@ -32,11 +32,11 @@ const SpacesMainImage = styled('images')(({ theme }) => ({
     },
 }));
 
-const SpaceSideImage = styled('images')(({ theme }) => ({
+const LocationSideImages = styled('images')(({ theme }) => ({
     
 }));
 
-function AvailableEquipment({/*will and to change this later to be more like OpeningImage*/}){
+function Location({}){
     const { ref, inView } = useInView({
         triggerOnce: true,
         rootMargin: '-170px 0px',
@@ -44,19 +44,19 @@ function AvailableEquipment({/*will and to change this later to be more like Ope
     return(
         <Grid sx={{mt:{ lg:1, md:1, xs:1 }}} container spacing={4} justifyContent="center" backgroundColor={"#cfcece"}>
             <Grid item md={12} xs={12} ref={ref}>
-                <AvailableEquipmentTitle className={inView ? 'slideIn1300ms' : 'hidden'}>
-                    Available Equipment
-                </AvailableEquipmentTitle>
+                <IlabTitle className={inView ? 'slideIn1300ms' : 'hidden'}>
+                    Location
+                </IlabTitle>
             </Grid>
             {/*problem with scrollable starting off the screen*/}
             <Grid className="horizontal-scrollable" item lg={12} md={12} xs={12} ref={ref}> 
-                    <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
+                    <LocationSideImages style={{backgroundImage:`url("${group_1}"`}} />
             </Grid>
         </Grid>
     );
 }
 
-const AvailableEquipmentTitle = styled('div')(({ theme }) => ({
+const IlabTitle = styled('div')(({ theme }) => ({
     fontWeight: "bold",
     wordSpacing: '-3px',
     letterSpacing: "-1px",
@@ -75,10 +75,13 @@ const AvailableEquipmentTitle = styled('div')(({ theme }) => ({
 const AvailableEquipmentImages = styled('div')(({ theme }) => ({
     backgroundSize: "cover",
     height: "250px",
+    width: "250px",
     [theme.breakpoints.down('lg')]: {
         height: "200px",
+        width: "200px",
         [theme.breakpoints.down('md')]: {
             height: "150px",
+            width: "150px",
         },
     },
 }));
@@ -97,14 +100,14 @@ const AvailableEquipmentBlock = styled('div')(({ theme }) => ({
     borderRadius:'10px',
     marginRight: "10px",
     [theme.breakpoints.down('lg')]: {
-        height: "45%",
-        width: "30%",
+        height: "300px",
+        width: "270px",
         [theme.breakpoints.down('md')]: {
-            height: "45%",
-            width: "30%",
+            height: "300px",
+            width: "220px",
             [theme.breakpoints.down('sm')]: {
-                height: "50%",
-                width: "40%",
+                height: "200px",
+                width: "175px",
             }
         },
     },
@@ -118,29 +121,29 @@ function AvailableEquipment({/*will and to change this later to be more like Ope
     return(
         <Grid sx={{mt:{ lg:1, md:1, xs:1 }}} container spacing={4} justifyContent="center" backgroundColor={"#cfcece"}>
             <Grid item md={12} xs={12} ref={ref}>
-                <AvailableEquipmentTitle className={inView ? 'slideIn1300ms' : 'hidden'}>
-                    Available Equipment
-                </AvailableEquipmentTitle>
+                <IlabTitle className={inView ? 'slideIn1300ms' : 'hidden'}>
+                    AVAILABLE EQUIPMENT
+                </IlabTitle>
             </Grid>
             {/*problem with scrollable starting off the screen*/}
             <Grid className="horizontal-scrollable" item lg={12} md={12} xs={12} ref={ref}> 
-                <AvailableEquipmentBlock className={inView ? 'slideIn1300ms' : 'hidden'} sx={{ml:3}}>
+                <AvailableEquipmentBlock>
                     {/*problem with element initially loading into screen (resizes after getting into position*/}
                     <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
                     <AvailableEquipmentText sx={{mt:2, fontWeight:'bold'}}>Esports Awards</AvailableEquipmentText>
                     <AvailableEquipmentText sx={{mt:1}}>Tempora aut est maiores error laudantium ut architecto</AvailableEquipmentText>
                 </AvailableEquipmentBlock>
-                <AvailableEquipmentBlock className={inView ? 'slideIn1300ms' : 'hidden'}>
+                <AvailableEquipmentBlock>
                     <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
                     <AvailableEquipmentText sx={{mt:2, fontWeight:'bold'}}>OWL Internship</AvailableEquipmentText>
                     <AvailableEquipmentText sx={{mt:1}}>Tempora aut est maiores error laudantium ut architecto</AvailableEquipmentText>
                 </AvailableEquipmentBlock>
-                <AvailableEquipmentBlock className={inView ? 'slideIn1300ms' : 'hidden'}>
+                <AvailableEquipmentBlock>
                     <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
                     <AvailableEquipmentText sx={{mt:2, fontWeight:'bold'}}>Esports Awards</AvailableEquipmentText>
                     <AvailableEquipmentText sx={{mt:1}}>Tempora aut est maiores error laudantium ut architecto</AvailableEquipmentText>
                 </AvailableEquipmentBlock>
-                <AvailableEquipmentBlock className={inView ? 'slideIn1300ms' : 'hidden'}>
+                <AvailableEquipmentBlock>
                     <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
                     <AvailableEquipmentText sx={{mt:2, fontWeight:'bold'}}>OWL Internship</AvailableEquipmentText>
                     <AvailableEquipmentText sx={{mt:1}}>Tempora aut est maiores error laudantium ut architecto</AvailableEquipmentText>
@@ -154,8 +157,9 @@ function AvailableEquipment({/*will and to change this later to be more like Ope
 function Ilab() {
     return (
         <>
-        <OpeningPageImage header="iLab" subtitle="Subtitle text" imageURL={group_4} />
-        <Spaces />
+        {/*gonna figure out how to have line breaks within the subtitle*/}
+        <OpeningPageImage header="ILab" subtitle=">>MONDAY - FRIDAY 10:30 AM - 4:00 PM HST"imageURL={group_4} />
+        <Location />
         <AvailableEquipment />
         </>
     );
