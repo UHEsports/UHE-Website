c399f8d9e9e78f16bdc57e31f58de24f9d8160dc
Kmiks <143069244+Kmiks@users.noreply.github.com>
2023-10-15 17:40:25 -1000

    half finished Location section
    Needs to be looked over again but sort of finished the location section. Decided to use 2 images instead of the initial 3 since I couldn't get 3 images to look good. Also deleted a lot of excess code and switched the "AvailableEquipmentTitle" to "SectionTitle" I imported along with others

--- src/pages/Ilab.js
+++ src/pages/Ilab.js
@@ -7,30 +7,17 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import './Ilab.css';
 import { styled } from '@mui/material/styles';
-import group_1 from '../images/group_1.jpg';
-import group_2 from '../images/group_2.jpg';
-import group_3 from '../images/group_3.jpg';
-import group_4 from '../images/group_4.png';
-import discord_server from '../images/discord_server.png';
-import Slider from "react-slick";
-import uheLogo from "../images/uhe_logo.png";
+import group_1 from '../assets/images/group_1.jpg';
+import group_2 from '../assets/images/group_2.jpg';
+import group_3 from '../assets/images/group_3.jpg';
+import group_4 from '../assets/images/group_4.png';
+import { useInView } from 'react-intersection-observer';
 import SectionTitle from "../components/SectionTitle";
 import SectionDescription from "../components/SectionDescription";
-import { useInView } from 'react-intersection-observer';
 import {Link as RouterLink} from "react-router-dom";
 import OpeningPageImage from '../components/OpeningPageImage';
 import { Block, ChildCare } from '@mui/icons-material';
 
-const LocationMainText = styled('images')(({ theme }) => ({
-    backgroundSize: "cover",
-    height: "250px",
-    [theme.breakpoints.down('lg')]: {
-        height: "200px",
-        [theme.breakpoints.down('md')]: {
-            height: "150px",
-        },
-    },
-}));
 /*
 const LocationSideImages = styled('images')(({ theme }) => ({
     backgroundSize: "cover",
@@ -64,89 +51,74 @@ function Location({}){
 }
 */
 
-function Location() {
-    function LocationSideImages({ imageUrl }) {
-        return (
-            <img src={imageUrl} alt="Image" className="LocationImageStyle" />
-        );
-    }
+const LocationSideImages = styled('div')(({ theme }) => ({
+    backgroundSize: "cover",
+    width: "100%",
+    height: "350px",
+    margin: "10px",
+    [theme.breakpoints.down('lg')]: {
+        height: "300px",
+        [theme.breakpoints.down('md')]: {
+            height: "250px",
+            [theme.breakpoints.down('xs')]: {
+                width: "100%",
+            },
+        },
+    },
+}));
 
+function Location() {
     return (
-        <Grid container spacing={4} justifyContent="center">
-            <Grid item xs={12} md={6}>
-                <div>
-                    <h1>Location</h1>
-                </div>
-                <div>
-                    {/* Text on the left */}
-                    <p>Your text on the left</p>
-                </div>
+        <Grid container spacing={2} justifyContent="center" sx={{mt: 2,}}>
+            <Grid item xs={12} md={5}>
+                <SectionTitle style={{textAlign:"left", fontSize: "5 rem",}} sx={{ml:3, mb: 4, }}>
+                    LOCATION
+                </SectionTitle>
+                <SectionDescription sx={{ml: 3, mr:3, mb: 3,}}>
+                &#8226; {/* <-- bulletpoint*/} <span style={{ fontWeight: 'bold' }}>Lorem ipsum - </span>
+                {" "}ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </SectionDescription>
+                <SectionDescription sx={{ml: 3, mr:3, mb: 3,}}>
+                &#8226; {/* <-- bulletpoint*/} <span style={{ fontWeight: 'bold' }}>Lorem ipsum - </span>
+                {" "}ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                </SectionDescription>
             </Grid>
-            <Grid item xs={12} md={6}>
-                <LocationSideImages imageUrl={group_1} />
-                <LocationSideImages imageUrl={group_2} />
-                <LocationSideImages imageUrl={group_3} />
+            <Grid item xs={12} s={6} md={6}>
+                <LocationSideImages style={{backgroundImage:`url("${group_1}"`}} s={6} md={7} />
+                <LocationSideImages style={{backgroundImage:`url("${group_1}"`}} s={6} md={7}/>
             </Grid>
         </Grid>
     );
 }
-const LocationTitle = styled('div')(({ theme }) => ({
-    fontWeight: "bold",
-    wordSpacing: '-3px',
-    letterSpacing: "-1px",
-    fontSize: "3.25rem",
-    textAlign: "left",
-    marginLeft: "1.5rem",
-    [theme.breakpoints.down('lg')]: {
-        fontSize: "2.5rem",
-        [theme.breakpoints.down('sm')]: {
-            fontSize: "2.25rem",
-        },
-    },
-}));
 
-const IlabTitle = styled('div')(({ theme }) => ({
-    fontWeight: "bold",
-    wordSpacing: '-3px',
-    letterSpacing: "-1px",
-    fontSize: "3.25rem",
-    textAlign: "center",
-    marginLeft: "1.5rem",
-    [theme.breakpoints.down('lg')]: {
-        fontSize: "2.5rem",
-        [theme.breakpoints.down('sm')]: {
-            fontSize: "2.25rem",
-        },
-    },
-}));
 
+/*needs centered in the equipmentblock*/
 const AvailableEquipmentImages = styled('div')(({ theme }) => ({
     backgroundSize: "cover",
-    height: "250px",
-    width: "250px",
+    height: "300px",
+    width: "300px",
     [theme.breakpoints.down('lg')]: {
-        height: "200px",
-        width: "200px",
+        height: "250px",
+        width: "250px",
         [theme.breakpoints.down('md')]: {
-            height: "150px",
-            width: "150px",
+            height: "200px",
+            width: "200px",
         },
-    },
-}));
-
-const AvailableEquipmentText = styled('div')(({ theme }) => ({
-    textAlign: 'left',
-    fontSize:"1.2rem",
-    overflow: "Hidden",
-    [theme.breakpoints.down('lg')]: {
-        fontSize: "1.0rem",
     },
 }));
 
 const AvailableEquipmentBlock = styled('div')(({ theme }) => ({
     backgroundSize: "cover",
-    borderRadius:'10px',
     marginRight: "10px",
+    border: "2px solid",/* <-- temporary*/
     height: "300px",
     width: "270px",
     [theme.breakpoints.down('lg')]: {
@@ -163,6 +135,12 @@ const AvailableEquipmentBlock = styled('div')(({ theme }) => ({
     },
 }));
 
+/*to change the overflow for the AvailableEquipmentDescription TEMPORARY */
+const AEDesc = {
+    marginTop: 1,
+    overflow: 'hidden',
+  };
+
 function AvailableEquipment({/*will and to change this later to be more like OpeningImage*/}){
     const { ref, inView } = useInView({
         triggerOnce: true,
@@ -171,38 +149,37 @@ function AvailableEquipment({/*will and to change this later to be more like Ope
     return(
         <Grid sx={{mt:{ lg:1, md:1, xs:1 }}} container spacing={4} justifyContent="center" backgroundColor={"#cfcece"}>
             <Grid item md={12} xs={12} ref={ref}>
-                <IlabTitle className={inView ? 'slideIn1300ms' : 'hidden'}>
+                <SectionTitle>
                     AVAILABLE EQUIPMENT
-                </IlabTitle>
+                </SectionTitle>
             </Grid>
             {/*problem with scrollable starting off the screen*/}
             <Grid className="horizontal-scrollable" item lg={12} md={12} xs={12} ref={ref}> 
                 <AvailableEquipmentBlock>
                     {/*problem with element initially loading into screen (resizes after getting into position*/}
                     <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
-                    <AvailableEquipmentText sx={{mt:2, fontWeight:'bold'}}>Esports Awards</AvailableEquipmentText>
-                    <AvailableEquipmentText sx={{mt:1}}>Tempora aut est maiores error laudantium ut architecto</AvailableEquipmentText>
+                    <SectionDescription sx={{mt:2, fontWeight:'bold'}}>Esports Awards</SectionDescription>
+                    <SectionDescription sx={AEDesc}>Tempora aut est maiores error laudantium ut architecto</SectionDescription>
                 </AvailableEquipmentBlock>
                 <AvailableEquipmentBlock>
                     <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
-                    <AvailableEquipmentText sx={{mt:2, fontWeight:'bold'}}>OWL Internship</AvailableEquipmentText>
-                    <AvailableEquipmentText sx={{mt:1}}>Tempora aut est maiores error laudantium ut architecto</AvailableEquipmentText>
+                    <SectionDescription sx={{mt:2, fontWeight:'bold'}}>OWL Internship</SectionDescription>
+                    <SectionDescription sx={AEDesc}>Tempora aut est maiores error laudantium ut architecto</SectionDescription>
                 </AvailableEquipmentBlock>
                 <AvailableEquipmentBlock>
                     <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
-                    <AvailableEquipmentText sx={{mt:2, fontWeight:'bold'}}>Esports Awards</AvailableEquipmentText>
-                    <AvailableEquipmentText sx={{mt:1}}>Tempora aut est maiores error laudantium ut architecto</AvailableEquipmentText>
+                    <SectionDescription sx={{mt:2, fontWeight:'bold'}}>Esports Awards</SectionDescription>
+                    <SectionDescription sx={AEDesc} >Tempora aut est maiores error laudantium ut architecto</SectionDescription>
                 </AvailableEquipmentBlock>
                 <AvailableEquipmentBlock>
                     <AvailableEquipmentImages style={{backgroundImage:`url("${group_1}"`}} />
-                    <AvailableEquipmentText sx={{mt:2, fontWeight:'bold'}}>OWL Internship</AvailableEquipmentText>
-                    <AvailableEquipmentText sx={{mt:1}}>Tempora aut est maiores error laudantium ut architecto</AvailableEquipmentText>
+                    <SectionDescription sx={{mt:2, fontWeight:'bold'}}>OWL Internship</SectionDescription>
+                    <SectionDescription sx={AEDesc}>Tempora aut est maiores error laudantium ut architecto</SectionDescription>
                 </AvailableEquipmentBlock>
             </Grid>
         </Grid>
     );
 }
-
 
 function Ilab() {
     return (
