cd4333e57e6aae337b1d1e3ffb00a16f51f86c1c
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2023-12-21 14:51:48 -1000

    added in scroll behavior and made it smooth

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,12 +1,47 @@
 import './App.css';
+import { IconButton } from "@material-ui/core";
 import Navbar from "../components/Navbar";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Ilab from "../pages/Ilab";
 import Education from "../pages/Education";
 import ContactUs from "../pages/ContactUs";
+import { useRef, useState, useEffect } from "react";
 
 function App() {
+    const ref = useRef();
+
+    const [pos, setPos] = useState(false);
+
+    const handleTop = () => {
+        window.scrollTo(0,0)
+        setPos(false);
+    };
+
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollHeight - Math.round(document.body.scrollTop) === document.body.clientHeight) {
+            setPos(true);
+        } else {
+            setPos(false);
+        }
+    }
+    // const handleScroll = () => {
+    //     if (
+    //         ref.current.scrollHeight - Math.round(ref.current.scrollTop) ===
+    //         ref.current.clientHeight
+    //     ) {
+    //         if (!pos) setPos(true);
+    //     } else {
+    //         if (pos) setPos(false);
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     const temp = ref.current;
+    //     temp.addEventListener("scroll", handleScroll);
+    //     return () => temp.removeEventListener("scroll", handleScroll);
+    // });
   return (
     <div className="App">
         <Router>
@@ -18,6 +53,17 @@ function App() {
                 <Route path="contact-us" element={<ContactUs />} />
             </Routes>
         </Router>
+        <IconButton
+            style={{
+                position: "fixed",
+                bottom: 10,
+                right: 10,
+                display: pos ? "block" : "none"
+            }}
+            onClick={handleTop}
+        >
+            Up
+        </IconButton>
     </div>
   );
 }
