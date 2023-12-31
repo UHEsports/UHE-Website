a393410279323811c7e49f4d0f9454ed38bd4708
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2023-12-30 17:18:01 -1000

    mobile footer is not correctly displayed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,73 +24,76 @@ const FooterText = styled('div')(({ theme }) => ({
 
 function Footer() {
     return(
-            <Grid container alignItems="center" sx={{pt:2, pb:4, backgroundColor: "#f6f6f6"}}>
-                <Box sx={{ml:2}}>
-                    <img
-                        alt="UHE Logo"
-                        width="150px"
-                        src={uheLogo}
-                        // style={{ float: "left" }}
-                    />
-                </Box>
-                <Box sx={{ flexGrow: 1 }} />
-                <nav>
-                    <Link
-                        underline="none"
-                        variant="button"
-                        color="black"
-                        rel="noopener noreferrer"
-                        href="https://www.twitch.tv/uh_esports"
-                        target="_blank"
-                        sx={{mx: 0.5 }}
-                    >
-                        <TwitchSolidIcon sx={{fontSize:40}} />
-                    </Link>
-                    <Link
-                        underline="none"
-                        variant="button"
-                        color="black"
-                        rel="noopener noreferrer"
-                        href="https://www.instagram.com/uhesports/"
-                        target="_blank"
-                        sx={{mx: 0.5 }}
-                    >
-                        <InstagramSolidIcon sx={{fontSize:40}} />
-                    </Link>
-                    <Link
-                        underline="none"
-                        variant="button"
-                        color="black"
-                        rel="noopener noreferrer"
-                        href="https://www.tiktok.com/@uhesports"
-                        target="_blank"
-                        sx={{mx: 0.5 }}
-                    >
-                        <TiktokSolidIcon sx={{fontSize:40}} />
-                    </Link>
-                    <Link
-                        underline="none"
-                        variant="button"
-                        color="black"
-                        rel="noopener noreferrer"
-                        href="https://discord.com/invite/UHEsports"
-                        target="_blank"
-                        sx={{mx: 0.5 }}
-                    >
-                        <DiscordSolidIcon sx={{fontSize:40}} />
-                    </Link>
-                    <Link
-                        underline="none"
-                        variant="button"
-                        color="black"
-                        rel="noopener noreferrer"
-                        href="https://twitter.com/UHEsport"
-                        target="_blank"
-                        sx={{ml: 0.5, mr:4 }}
-                    >
-                        <TwitterIcon sx={{fontSize:40}} />
-                    </Link>
-                </nav>
+            <Grid container sx={{pt:2, pb:4, backgroundColor: "#f6f6f6", display:'block', alignItems: "center"}}>
+               {/*<Grid lg={12} alignItems="center" sx={{display:'flex'}}>*/}
+               {/*<Box sx={{ml:2}}>*/}
+                   <img
+                       alt="UHE Logo"
+                       width="150px"
+                       src={uheLogo}
+                       style={{ float: "left" }}
+                   />
+               {/*</Box>*/}
+               {/*<Box sx={{ flexGrow: 1 }} />*/}
+               <nav style={{float:"right"}}>
+                   <Link
+                       underline="none"
+                       variant="button"
+                       color="black"
+                       rel="noopener noreferrer"
+                       href="https://www.twitch.tv/uh_esports"
+                       target="_blank"
+                       sx={{mx: 0.5 }}
+                   >
+                       <TwitchSolidIcon sx={{fontSize:40}} />
+                   </Link>
+                   <Link
+                       underline="none"
+                       variant="button"
+                       color="black"
+                       rel="noopener noreferrer"
+                       href="https://www.instagram.com/uhesports/"
+                       target="_blank"
+                       sx={{mx: 0.5 }}
+                   >
+                       <InstagramSolidIcon sx={{fontSize:40}} />
+                   </Link>
+                   <Link
+                       underline="none"
+                       variant="button"
+                       color="black"
+                       rel="noopener noreferrer"
+                       href="https://www.tiktok.com/@uhesports"
+                       target="_blank"
+                       sx={{mx: 0.5 }}
+                   >
+                       <TiktokSolidIcon sx={{fontSize:40}} />
+                   </Link>
+                   <Link
+                       underline="none"
+                       variant="button"
+                       color="black"
+                       rel="noopener noreferrer"
+                       href="https://discord.com/invite/UHEsports"
+                       target="_blank"
+                       sx={{mx: 0.5 }}
+                   >
+                       <DiscordSolidIcon sx={{fontSize:40}} />
+                   </Link>
+                   <Link
+                       underline="none"
+                       variant="button"
+                       color="black"
+                       rel="noopener noreferrer"
+                       href="https://twitter.com/UHEsport"
+                       target="_blank"
+                       sx={{ml: 0.5, mr:4 }}
+                   >
+                       <TwitterIcon sx={{fontSize:40}} />
+                   </Link>
+               </nav>
+               {/*</Grid>*/}
+
                 <Grid container spacing={3} sx={{ display: { lg: "flex", md: "flex", sm: "none", xs: "none" } }}>
                     <Grid sx={{alignItems: 'center'}} lg={5} md={6}>
                         <FooterText sx={{textAlign:'left', color:'grey', ml:7}}>
@@ -107,8 +110,7 @@ function Footer() {
                     </Grid>
                     <Grid sx={{float:'right'}} lg={2} md={4}>
                         <FooterText sx={{textAlign:'right', color:'grey', mr:4}}>
-                            2532 Correa Rd,
-                            <br/>
+                            2532 Correa Rd,<br/>
                             Honolulu, HI 96822
                         </FooterText>
 
