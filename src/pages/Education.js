544e99eb97b6187d9101c3f8aa2421b8d84b5c49
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2024-01-14 16:32:26 -1000

    added additional comment to the Education.js page

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -151,6 +151,7 @@ function Courses() {
         rootMargin: '-150px',
     });
     return(
+        // TODO remove the mb spacing. Only added to showcase footer spacing
         <Grid sx={{mt:8, backgroundColor:"#f6f6f6", pt:1, pb:5, mb:6}} spacing={4} container justifyContent="center" alignItems="center" ref={ref}>
             <Grid item lg={5} md={5} sm={11} xs={11}>
                 <SectionTitle className={inView ? 'slideIn1300ms' : 'hidden'}>Courses</SectionTitle>
