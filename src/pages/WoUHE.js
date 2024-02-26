02a87e0a88807937b9bdf74295ded1dbe3302ba5
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2024-02-26 12:43:58 -1000

    changed the rotation of the text for the WoUHE teams section

--- src/pages/WoUHE.js
+++ src/pages/WoUHE.js
@@ -10,41 +10,42 @@ import valorantWoUHE from "../assets/images/valorant_wouhe.png";
 import wraith from "../assets/images/wraith_apex.png";
 import battlefield from "../assets/images/battlefield_mlbb_1.png";
 
-const WoUHEOpeningImage = styled('div')(({ theme }) => ({
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    height: "700px",
-    borderRadius:'10px',
-    [theme.breakpoints.down('lg')]: {
-        height: "400px",
-        [theme.breakpoints.down('md')]: {
-            height: "250px",
-        },
-    },
-}));
+// TODO remove if the img tag works better
+// const WoUHEOpeningImage = styled('div')(({ theme }) => ({
+//     backgroundSize: "cover",
+//     backgroundPosition: "center",
+//     height: "700px",
+//     borderRadius:'10px',
+//     [theme.breakpoints.down('lg')]: {
+//         height: "400px",
+//         [theme.breakpoints.down('md')]: {
+//             height: "250px",
+//         },
+//     },
+// }));
 function AboutWoUHE() {
     return (
         <Grid container sx={{ mt:{ lg:'182px', md:'182px', sm:'110px', xs:'100px' }}} justifyContent="center">
-            <Grid item md={12} sm={12} xs={12}>
+            <Grid item lg={12} md={12} sm={12} xs={12}>
                 <img
                     alt="Women of UHE Logo"
-                    width="115"
+                    width="95"
                     src={woUHELogo}
                 />
             </Grid>
-            <Grid item md={12} sm={12} xs={12} sx={{ mt:{ lg:2, md:2 }}}>
+            <Grid item lg={12} md={12} sm={12} xs={12} sx={{ mt:{ lg:1, md:1, sm:1, xs:1 }}}>
                 <SectionTitle>
                     Women of UHE
                 </SectionTitle>
             </Grid>
-            <Grid item lg={7} md={8} sm={10} xs={11} sx={{ mt:{ lg:1, md:1 }, p:2,}}>
+            <Grid item lg={7} md={8} sm={10} xs={11} sx={{ mt:{ lg:3, md:3, sm:3, xs:2 }, px:2, pb:2 }}>
                 <SectionDescription sx={{textAlign:'center'}}>
                     Talk about the team’s history here.
                     Duis in nulla ullamco cillum aliqua et incididunt in culpa tempor cupidatat irure consectetur est aliquip dolor, amet.
                 </SectionDescription>
             </Grid>
-            <Grid item lg={8} md={9} sm={11} xs={11} sx={{ mt:{ lg:5, md:4 }}}>
-                <img src={group_1} width="100%" style={{borderRadius:'10px'}}></img>
+            <Grid item lg={8} md={10} sm={11} xs={11} sx={{ mt:{ lg:5, md:4, sm:4, xs:2 }}}>
+                <img src={group_1} alt="Women of UHE Group photo" width="100%" style={{borderRadius:'10px'}}></img>
             </Grid>
         </Grid>
     );
@@ -56,29 +57,35 @@ const GameCard = styled('div')(({theme, defaultBackground}) => ({
     borderRadius: '10px',
     position: 'relative',
     backgroundSize: '800px',
-    backgroundPosition: "-130px",
-    width: '634px !important',
-    height: '450px',
+    backgroundPosition: "-150px",
+    // backgroundPosition:"center",
+    // width: '634px !important',
+    width:"100%",
+    height: '425px',
     // TODO add in mobile conversion
-    // [theme.breakpoints.down('lg')]: {
-    //     width: '115px !important',
-    //     height: '400px',
+    [theme.breakpoints.down('lg')]: {
+        width: '100%',
+        height: '300px',
+        backgroundSize: '650px',
+        backgroundPosition: "-160px",
     //     [theme.breakpoints.down('sm')]: {
     //         width: '105px !important',
     //         height: '400px',
     //     },
-    // },
+    },
 }));
 
 const GameTitleText = styled('div')(({ theme }) => ({
     fontWeight: 'bold',
-    fontSize: "40px",
-    textAlign:'left',
-    transform: 'rotate(-90deg)',
-    position:'absolute',
-    width: '420px',
-    top: '45%',
-    left:'390px',
+    fontSize: "38px",
+    textAlign:'right',
+    // transform: 'rotate(-90deg)',
+    writingMode: 'vertical-lr',
+    position:'relative',
+    height: '100%',
+    bottom: '3%',
+    // left:'91%',
+    float:'right',
     // TODO add in mobile conversion
     // [theme.breakpoints.down('lg')]: {
     //     fontSize: "2.30rem",
@@ -95,13 +102,13 @@ const GameTitleText = styled('div')(({ theme }) => ({
 
 function WoUHEGames() {
     return(
-        <Grid sx={{mt:{ lg:4, md:1, xs:1 }}} spacing={4} container justifyContent="center">
+        <Grid sx={{mt:{ lg:4, md:1, xs:1 }}} spacing={2} container justifyContent="center">
             <Grid item md={12} xs={12}>
                 <SectionTitle>
                     Our Teams
                 </SectionTitle>
             </Grid>
-            <Grid sx={{mt:{ lg:2, md:1, xs:1 }}} item md={4} xs={11}>
+            <Grid sx={{mt:{ lg:2, md:1, xs:1 }}} item lg={4} md={5} xs={11}>
                 <GameCard defaultBackground={valorantWoUHE}>
                     {/*<ApexCharacterImage />*/}
                     <GameTitleText>
@@ -109,7 +116,7 @@ function WoUHEGames() {
                     </GameTitleText>
                 </GameCard>
             </Grid>
-            <Grid sx={{mt:{ lg:2, md:1, xs:1 }}} item md={4} xs={11}>
+            <Grid sx={{mt:{ lg:2, md:1, xs:1 }}} item lg={4} md={5} xs={11}>
                 <GameCard defaultBackground={leagueWoUHE}>
                     {/*<ApexCharacterImage2 />*/}
                     <GameTitleText>
